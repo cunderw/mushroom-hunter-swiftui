fe2645c86d729a97184b6dc1dd8ab4364378becf
Carson Underwood <cunderw@gmail.com>
2024-02-13 09:34:35 -0600

    Initial Commit

---
+++ mymushroomshunter/mymushroomshunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  mymushroomshunterApp.swift
+//  mymushroomshunter
+//
+//  Created by Carson Underwood on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct mymushroomshunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mymushroomshunterUITests/mymushroomshunterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mymushroomshunterUITestsLaunchTests.swift
+//  mymushroomshunterUITests
+//
+//  Created by Carson Underwood on 2/13/24.
+//
+
+import XCTest
+
+final class mymushroomshunterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
